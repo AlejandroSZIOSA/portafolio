be1a97ba55cd732609be54af366532d1993b7646
AlejandroSZIOSA <ggato33@gmail.com>
2024-02-06 11:08:54 +0100

    some changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
-import "@/styles/globals.css";
+import { useEffect } from "react";
+import { ThemeProvider, useTheme } from "@/components/ThemeContex";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
+  const { state } = useTheme();
+
+  /* useEffect(() => {
+    // Your code here. Add the dark class to the <html /> tag
+    // with vanilla js.
+    const bodyEl = document.querySelector("html");
+    bodyEl.classList.add("dark");
+  }, [state.theme]); */
+
   return <Component {...pageProps} />;
 }
+
+/* export default ({ Component, pageProps }) => (
+  <ThemeProvider>
+    <MyApp Component={Component} pageProps={pageProps} />
+  </ThemeProvider>
+);
+ */

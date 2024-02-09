deceaf88361de418c2b0a2139724aeb39f9f7cfa
AlejandroSZIOSA <ggato33@gmail.com>
2024-02-09 10:12:21 +0100

    Context and reducers obj

--- src/components/ThemeDemo.js
+++ src/components/ThemeDemo.js
@@ -1,3 +1,25 @@
+import { useThemeContext } from "@/utils/context/ThemeContext";
+
 export default function ThemeDemo() {
-  return <div></div>;
+  const { state, dispatch } = useThemeContext();
+
+  function handleToLightMode() {
+    dispatch({ type: "toLight", background: "white", text: "black" });
+  }
+
+  function handleToDarkMode() {
+    dispatch({ type: "toDark", background: "white", text: "black" });
+  }
+
+  return (
+    <>
+      <h1>Context test Component</h1>
+      <button onClick={handleToLightMode}>To Light</button>
+      <br />
+      <button onClick={handleToDarkMode}>To Dark</button>
+      <p>
+        {state.background} {state.text}
+      </p>
+    </>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,13 @@
-import { useThemeContext } from "@/utils/context/ThemeContext";
+import ThemeDemo from "@/components/ThemeDemo";
 
 export default function Home() {
-  const { state, dispatch } = useThemeContext();
-
-  function handleToLightMode() {
-    dispatch({ type: "toLight", background: "white", text: "black" });
-  }
-
   /* console.log("hola"); */
   return (
     <main>
       <div>
         test1
         <p>Index</p>
-        <button onClick={handleToLightMode}>To Light</button>
-        <p>{state.background}</p>
+        <ThemeDemo />
       </div>
     </main>
   );

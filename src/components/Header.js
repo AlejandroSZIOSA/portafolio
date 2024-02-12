06b3dc3a25febe5de7d3990ca28740df3c20024c
AlejandroSZIOSA <ggato33@gmail.com>
2024-02-12 10:08:49 +0100

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div style={{ background: "red" }}>
+    <div>
       <div></div>
       Header
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,26 @@ import ThemeDemo from "@/components/ThemeDemo";
 
 export default function Home() {
   /* console.log("hola"); */
+  const navBox = "";
+
   return (
-    <main>
-      <div>
-        test1
-        <p>Index</p>
-        <ThemeDemo />
-      </div>
-    </main>
+    <div id="mainContainer" className="flex flex-col items-center ">
+      <header className="flex flex-col w-full bg-black">
+        <h1 className="text-white">Portafolio</h1>
+        <nav className="flex flex-row text-red-500">
+          <ol className="inline-flex gap-3">
+            <a>Start</a>
+            <a>Skills</a>
+            <a>projects</a>
+            <a>Contact</a>
+          </ol>
+        </nav>
+      </header>
+      <main>
+        {/*     <div>
+          <ThemeDemo />
+        </div> */}
+      </main>
+    </div>
   );
 }

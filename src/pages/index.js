95df8fa2f4c7edbead07ee062c45c7d46ba8f3b5
AlejandroSZIOSA <ggato33@gmail.com>
2024-02-12 13:05:11 +0100

    Navigation Bar work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,25 @@
-import ThemeDemo from "@/components/ThemeDemo";
+const navItemBoxes = "w-[83px] h-9 bg-[#444444] rounded-sm underline";
 
 export default function Home() {
-  /* console.log("hola"); */
-  const navBox = "";
-
   return (
-    <div id="mainContainer" className="flex flex-col items-center ">
-      <header className="flex flex-col w-full bg-black">
-        <h1 className="text-white">Portafolio</h1>
-        <nav className="flex flex-row text-red-500">
-          <ol className="inline-flex gap-3">
-            <a>Start</a>
-            <a>Skills</a>
-            <a>projects</a>
-            <a>Contact</a>
-          </ol>
+    <div id="mainContainer" className="flex flex-col items-center">
+      <header className="flex flex-col w-full bg-[#343434] items-center ">
+        <h1 className="text-[#F24E1E] my-3 text-6xl">Portafolio</h1>
+        <nav className="p-2">
+          <ul className="flex gap-2 text-2xl text-[#00D8FF] list-none m-0 p-0">
+            <li className={navItemBoxes}>
+              <a>Start</a>
+            </li>
+            <li className={navItemBoxes}>
+              <a>Skills</a>
+            </li>
+            <li className={navItemBoxes}>
+              <a>Projects</a>
+            </li>
+            <li className={navItemBoxes}>
+              <a>Contact</a>
+            </li>
+          </ul>
         </nav>
       </header>
       <main>

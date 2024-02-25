be15dc689ff4512557f7575e25eccb91e512922f
AlejandroSZIOSA <ggato33@gmail.com>
2024-02-25 19:18:19 +0100

    some changes

--- src/components/main/HeroSection.js
+++ src/components/main/HeroSection.js
@@ -3,8 +3,8 @@ export default function HeroSection() {
     <section id="heroSection" className="flex flex-col items-center">
       <div className="size-80 bg-white"></div>
       <p className="text-4xl">
-        Hi! My name is <strong>Gabriel Sazo </strong>I am learning build
-        Frontend aplications for Web and Mobile.
+        Hi! My name is <strong>Gabriel Sazo </strong>I am Frontend Developer
+        aspirant.
       </p>
     </section>
   );
